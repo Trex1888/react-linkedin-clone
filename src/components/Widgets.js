588a931bc1fc3ff74e2f2fff4e74b8aa6a816b93
Trex1888 <harpua123@gmail.com>
2021-01-09 05:33:16 -0600

    2nd commit

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,17 +1,33 @@
 import React from "react";
 import "../styles/Widgets.css";
-import { Info } from "@material-ui/icons";
+import { FiberManualRecord, Info } from "@material-ui/icons";
 
 function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecord />
+      </div>
+
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="widgets">
       <div className="widgets__header">
         <h2>LinkedIn News</h2>
         <Info />
       </div>
+      {newsArticle(
+        "Hello",
+        "How's it goingasdfasdfasdfasd asdfasdf asdfaf fadfasdddd?"
+      )}
     </div>
   );
 }
 
 export default Widgets;
-//354
